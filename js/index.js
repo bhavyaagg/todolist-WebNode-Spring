4558e725502dcfa172f6e493e799e89b6cc36291
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-03-10 18:14:47 +0530

    Load todos after page refresh
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- js/index.js
+++ js/index.js
@@ -10,40 +10,89 @@ $(document).ready(function () {
   let addTodoBtn = $("#addTodo");
   let clearTodosBtn = $("#clearTodos");
   
-  let savedTodos = localStorage.getItem("todos");
+  retrieveTodos();
   
-  if (savedTodos) {
-    let todos = JSON.parse(savedTodos)
-    console.log(todos);
+  for (i in todos) {
+    let todoItem = createTodoItem(+i);
+    todoList.append(todoItem);
   }
   
   addTodoBtn.click(function () {
-    let newTodoValue = newTodoInput.val();
-    
-    todos.push({
-      task: newTodoValue,
-      done: false
-    });
-    
-    localStorage.setItem("todos", JSON.stringify(todos));
-    
-    let todoItem = $(`<li class="list-group-item"></li>`)
-    
-    let contentDiv = $(`<div class="row"></div>`)
-    
-    let checkBoxDiv = $(`<div class="col-2 text-center"></div>`)
-    
-    let check = $(`<input type="checkbox">`).click(function () {
-      contentDiv.css("text-decoration", "line-through");
-    })
-    
-    let task = $(`<div class="col">${newTodoValue}</div>`)
-    
-    contentDiv.append(checkBoxDiv.append(check)).append(task)
-    
-    todoItem.append(contentDiv)
-    
-    todoList.append(todoItem)
-  });
+    addTodo(newTodoInput.val());
+  })
+  
+  // addTodoBtn.click(function () {
+  //   let newTodoValue = newTodoInput.val();
+  //
+  //   todos.push({
+  //     task: newTodoValue,
+  //     done: false
+  //   });
+  //
+  //   localStorage.setItem("todos", JSON.stringify(todos));
+  //
+  //   let todoItem = $(`<li class="list-group-item"></li>`)
+  //
+  //   let contentDiv = $(`<div class="row"></div>`)
+  //
+  //   let checkBoxDiv = $(`<div class="col-2 text-center"></div>`)
+  //
+  //   let check = $(`<input type="checkbox">`).click(function () {
+  //     contentDiv.css("text-decoration", "line-through");
+  //   })
+  //
+  //   let task = $(`<div class="col">${newTodoValue}</div>`)
+  //
+  //   contentDiv.append(checkBoxDiv.append(check)).append(task)
+  //
+  //   todoItem.append(contentDiv)
+  //
+  //   todoList.append(todoItem)
+  // });
   
 });
+
+
+function createTodoItem(i) {
+  let todoItem = $(`<li class="list-group-item"></li>`)
+  
+  let contentDiv = $(`<div class="row"></div>`)
+  
+  let checkBoxDiv = $(`<div class="col-2 text-center"></div>`)
+  
+  let check = $(`<input type="checkbox" ${todos[i].done ? 'checked' : ''}>`).click(function () {
+    contentDiv.css("text-decoration", "line-through");
+  })
+  
+  if (todos[i].done) {
+    check.prop("checked", "true");
+  }
+  
+  let task = $(`<div class="col">${todos[i].task}</div>`)
+  
+  contentDiv.append(checkBoxDiv.append(check)).append(task)
+  
+  todoItem.append(contentDiv)
+  
+  return todoItem;
+}
+
+function retrieveTodos() {
+  let savedTodos = localStorage.getItem("todos");
+  if (savedTodos) {
+    todos = JSON.parse(savedTodos);
+  }
+}
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function addTodo(task) {
+  todos.push({
+    task: task,
+    done: false
+  });
+  saveTodos();
+  refreshTodos();
+}
